6ecd5181fe8c788a8e2a7ab30724142c3d34d5bd
Paul Bailey <paul.m.bailey@gmail.com>
2018-06-20 01:11:03 +0000

    question night

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
 
 var app = express();
 
+app.use(cors());
 app.use(express.static('build'));
 
-app.get('/api/get-questions', function (req, res) {
-  res.json({questions: []});
+app.get('/api/get-questions', function (req, res, next) {
+  axios.get('https://opentdb.com/api.php?amount=10')
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(next);
+});
+
+app.post('/login', function (req, res) {
+  // check username & password
+  // generate token
+  // res.json({jwt: token});
 });
 
 app.listen(9090, function () {
--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+function Narf (props) {
+  console.log(props);
+  return <div>Hi</div>;
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.api_count = 0;
+    
+    this.state = {
+      questions: [],
+      fname: 'Paul',
+      lname: 'B'
+    };
+    this.get_questions();
+  }
+  
+  get_questions(event) {
+    console.log(event);
+    this.api_count += 1;
+    
+    axios.get(
+      'http://45.76.237.114:9090/api/get-questions'
+      //{headers: {'JWT': localStorage.jwt}}
+    )
+     .then((response) => {
+       this.setState({
+         questions: response.data.results,
+         fname: 'Dude'
+       });
+     })
+     .catch(function () {
+       // put error modal
+     });
+  }
+  
   render() {
+    var narf = "First &middot; Second";
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +53,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={(e) => this.get_questions(e)}>New Questions</button>
+        <Narf fname={this.state.fname} lname={this.state.lname}/>
       </div>
     );
   }
